88ca1add491d0ebfdb6adfdbb622d64584cc1d26
fmitei <faith.mitei@trustgro.com>
2023-03-20 09:36:46 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+require 'pry'
+
+def greet_programmer
+puts "Hello, programmer!"
+end 
+greet_programmer
+
+
+def greet(name="Naureen")
+    puts "Hello, #{name}!"
+end
+greet
+
+def greet_with_default(name ="programmer")
+puts "Hello, #{name}!"
+end 
+greet_with_default
+
+def add(a,b)
+    return a+b 
+end
+add(2,2)
+
+def halve(a)
+    
+    if a.class == String
+        return nil
+    end 
+   return (a/2 )
+end 
+
+
+
